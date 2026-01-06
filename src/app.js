958d13561f97dda2694051a87d9f957eaf9c059e
RohitS0612 <shetakeyash@gmail.com>
2026-01-06 20:46:20 +0530

    setup of project

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import connectDB from "./config/DB.js";
 
 
 configDotenv();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
-
-
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server runing on: ${PORT}`)
 });
